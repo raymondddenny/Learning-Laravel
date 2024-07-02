1b35cee9a794a5e9fa56ac9fed88d7556c512c71
dennyraymondcashenable <denny.raymon@cashenable.com>
2024-07-02 22:43:56 +0700

    finish nav link

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>About Page</x-slot>
+    <h2>Hello From About Page</h2>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>Contact Page</x-slot:heading>
+    <h2>Hello From Contact Page</h2>
+</x-layout>
